9de6ac93aab187de6c54c68384ddbb9f9f0d921e
Wlad Meixner <hi@iamwlad.com>
2023-10-10 04:01:15 +0200

    fix: remove legacy dataset parsing

--- src/routes/graph/+page.server.ts
+++ src/routes/graph/+page.server.ts
@@ -48,54 +48,8 @@ export type Dataset = FsFile | FsDirectory;
 const datasetPath = '/static/dataset';
 
 export const load: Load = async ({ params }) => {
-	// Use glob to index all available data entries
-	const definitions = await import.meta.glob('/static/dataset/*/*.json');
-	// const data = await import.meta.glob('/static/datasets/*/*.csv');
-
 	const dataset = parseDataset(`.${datasetPath}`);
 
-	// Translate dataset file tree into comparable dataset items
-	// this is dataset specific and only intended to work for tum-db/partition-filters
-
-	const dataEntries: DataEntry[] = [];
-	const filters: { [key: string]: FilterEntry } = {};
-
-	// // Iterate over found modules
-	// for (const path in definitions) {
-	// 	// Simplit path into [filter]/[name] and remove extension
-	// 	let [filter, name] = path
-	// 		.split('/')
-	// 		.slice(-2)
-	// 		.map((p) => p.replace('.json', ''));
-
-	// 	filter = filter.replace('_part', '');
-
-	// 	// load defintiion info
-	// 	const info: EntryDefinition = (await definitions[path]()) as EntryDefinition;
-
-	// 	// console.log(info.name);
-
-	// 	const entry = {
-	// 		name: info.name,
-	// 		// FIXME: just a temporary hack to get the filter name -> add this value to test output
-	// 		filterName: info.name.split(' ')[0],
-	// 		fixture: info.fixture,
-	// 		infoUrl: `${base}/datasets/${filter}/${name}.json`,
-	// 		dataUrl: `${base}/datasets/${filter}/${name}.csv`
-	// 	};
-
-	// 	dataEntries.push(entry);
-
-	// 	// Construct build time filters
-	// 	if (!filters[filter]) {
-	// 		filters[filter] = {
-	// 			name: filter,
-	// 			entries: [entry]
-	// 		};
-	// 	} else {
-	// 		filters[filter].entries.push(entry);
-	// 	}
-	// }
 	return {
 		dataset
 		// data: dataEntries,
